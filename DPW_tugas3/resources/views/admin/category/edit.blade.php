38af4e82638d20e780cdbd342618bacae4ce17fd
Anggita Afrelia <afrelianieanggieta@gmail.com>
2022-01-19 18:21:30 +0700

    Backend

---
+++ DPW_tugas3/resources/views/admin/category/edit.blade.php
@@ -0,0 +1,41 @@
+@extends('template_admin.home')
+@section('sub_judul','Edit kategori')
+@section('content')
+
+
+@if(count($errors)>0)
+	@foreach($errors->all() as $error)
+<div class="alert alert-danger" role="alert">
+  {{ $error }}
+</div>
+	@endforeach
+@endif
+
+
+@if(Session::has('success'))
+<div class="alert alert-success" role="alert">
+  {{ Session('success')}}
+</div>
+@endif
+
+
+
+<form action="{{ route('category.update', $category->id) }}" method="POST">
+@csrf
+@method('patch')
+
+<br>
+	<div class="input-group input-group-outline">
+		<label class="form-label">Kategori</label>
+		<input type="text" class="form-control" name="name" value="{{ $category->name }}">
+	</div>
+
+<br>
+
+	<div class="input-group input-group-outline">
+		<button class="btn btn-primary btn-block">Edit Kategori</button>
+	</div>
+
+</form>
+
+@endsection
---
+++ DPW_tugas3/resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('template_admin.home')
+@section('sub_judul','Dashboard')
+@section('content')
+<h1>ini percobaan</h1>
+@endsection
